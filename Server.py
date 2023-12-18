13ee6f54687b0382d535a7f042d9a3df4158da12
jahanvvi <jahanviarora92@gmail.com>
2023-12-19 00:39:23 +0530

    Task 2: Server Side Updated

--- Server.py
+++ Server.py
@@ -3,10 +3,10 @@ from vidstream import AudioReceiver
 import time
 import threading
 
-Sender=AudioSender('192.168.36.178',6669)
+Sender=AudioSender('192.168.36.22',6669)
 Sender_thread= threading.Thread(target=Sender.start_stream)
 
-receiver=AudioReceiver('192.168.36.22',6669)
+receiver=AudioReceiver('192.168.36.178',6669)
 receiver_thread= threading.Thread(target=receiver.start_server())
 
 receiver_thread.start()
